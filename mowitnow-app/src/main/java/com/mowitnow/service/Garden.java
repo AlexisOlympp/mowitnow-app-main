1adfe454402a9ee676f27465010c1ef55d90ae59
Alexis MATHIEU <alexis.mathieu@olympp.fr>
2024-04-24 11:43:44 +0200

    add Garden model

---
+++ mowitnow-app/src/main/java/com/mowitnow/service/Garden.java
@@ -0,0 +1,30 @@
+package com.mowitnow.service;
+
+public class Garden {
+	private final int MIN_X = 0;
+    private final int MIN_Y = 0;
+    
+    private int maxX;
+    private int maxY;
+    
+	public int getMaxX() {
+		return maxX;
+	}
+	public void setMaxX(int maxX) {
+		this.maxX = maxX;
+	}
+	public int getMaxY() {
+		return maxY;
+	}
+	public void setMaxY(int maxY) {
+		this.maxY = maxY;
+	}
+	public int getMIN_X() {
+		return MIN_X;
+	}
+	public int getMIN_Y() {
+		return MIN_Y;
+	}
+    
+    
+}
